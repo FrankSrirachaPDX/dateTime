0a754f67bd8e0abfc7598b90f22fbbc8bd9bdf50
FrankSrirachaPDX <42188580+FrankSrirachaPDX@users.noreply.github.com>
2018-11-02 17:09:01 -0700

    Add files via upload

---
+++ dateTime/dateTime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime =  DateTime.Now;
+            int hours;
+
+            Console.WriteLine(dateTime);
+            Console.WriteLine("Enter a number!");
+            hours = Convert.ToInt32(Console.ReadLine());
+            dateTime = dateTime.AddHours(hours);
+            Console.WriteLine(dateTime);
+            Console.ReadLine();
+        }
+    }
+}
